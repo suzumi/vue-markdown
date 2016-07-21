e1a8373814998ce222df4e47b0a747de887971bb
suzumi <ganimata39@gmail.com>
2016-07-21 19:39:39 +0900

    first commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,63 @@
-import {Person} from './model/Person';
+// import {Person} from './model/Person';
+import Vue from 'vue';
 
-global.app = function () {
-    var christoph = new Person('Christoph', 'Burgdorf');
-    console.log(christoph.fullName);
-};
+class Human {
+    constructor(arg = { firstName: "John", lastName: "Doe" }) {
+        this.fields = arg;
+    }
+
+    get(fieldName) {
+        return this.fields[fieldName];
+    }
+
+    set(fieldName, value) {
+        this.fields[fieldName] = value;
+        return this;
+    }
+}
+
+// class Demo extends Vue {
+//     constructor() {
+//         var properties = {
+//             el: '#demo',
+//             data: {
+//                 bob: new Human({ firstName: "テスト", lastName: "太郎" })
+//             }
+//         };
+//         super(properties)
+//     }
+// }
+
+class Markdown extends Vue {
+    constructor() {
+        var properties = {
+            el: '#editor',
+            data: {
+                input: '#hello'
+            },
+            filters: {
+                marked: marked
+            }
+        };
+        super(properties)
+    }
+}
+
+// class HumansList extends Vue {
+//   constructor (collection) {
+//     this.collection = collection;
+//     super({
+//       el: "#humans-list", data: collection
+//     })
+//   }
+// }
+
+// let demo = new Demo()
+let md = new Markdown()
+
+// let humansList = new HumansList({
+//   humans:[
+//     new Human(),
+//     new Human({firstName:"Jane", lastName:"Doe"})
+//   ]
+// });
