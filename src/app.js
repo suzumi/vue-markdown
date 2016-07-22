16f3377015b460b4ff7e5d4e212204c055d9721c
suzumi <ganimata39@gmail.com>
2016-07-22 19:09:07 +0900

    supported mathML

--- src/app.js
+++ src/app.js
@@ -2,23 +2,36 @@
 import Vue from 'vue'
 import mdit from "markdown-it"
 import mdEmoji from "markdown-it-emoji"
+import mdHighlite from "markdown-it-highlighted"
+import markdownMath from "markdown-it-math"
 
-const markdown = new mdit().use(mdEmoji)
+const LaTeXStyle = {
+            inlineOpen: '\\(',
+            inlineClose: '\\)',
+            blockOpen: '\\[',
+            blockClose: '\\]'
+        }
+const markdown = new mdit().use(mdEmoji).use(markdownMath, LaTeXStyle)
 
-Vue.filter('md2html',  function(text) {
+Vue.filter('md2html', function (text) {
     return markdown.render(text);
-  });
+});
 
 class Markdown extends Vue {
     constructor() {
         var properties = {
             el: '#editor',
             data: {
-                input: '## Edit here!\nYou can use emoji :smile:'
+                input: '## Edit here!\nYou can use emoji :smile:\n\nand you can write block math\n\\[ sum_(n=0)^k a_n = a_0 + a_i + cdots + a_k\\]\n\nSee [ascii2mathml](http://runarberg.github.io/ascii2mathml/) for reference'
             }
         };
         super(properties)
     }
+
+    outConsole(text) {
+        console.log(text)
+    }
 }
 
 let md = new Markdown()
+md.outConsole(markdownMath)
